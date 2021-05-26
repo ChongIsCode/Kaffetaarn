49d19b9035fbb7b51e4553eca85d5176725cf6d2
Bjaef <seanillner@gmail.com>
2021-05-26 13:57:28 +0200

    Fixede umiddelbart at billedet røg under navbar og centrerede hero__text mere optimalt

---
+++ aabningstider.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kaffetår'n</title>
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1
+    ,600;1,700;1,800&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,500;0,600;0,700;0,800;0,900;1,400;
+    1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<?php include 'include/menu.php'?>
+
+<section class="hero">
+    <div class="hero__image">
+        <div class="hero__text">
+            <h1>Hvor er vi?</h1>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="text-element">
+        <h2>Åbningstider</h2>
+        <p>Kaffetår’n er en lokal café i hjertet af Nykøbing Falster, i det gamle vandtårn fra 1908.
+            Caféen bliver drevet af Dea Guttman, som åbnede dørene for første gang den 1. april 2020.
+            Kaffetår'n lægger meget vægt på at bruge lokale kvalitets råvare til at lave sine mange
+            hjemmelavede retter og kager. Kaffetår'n sælger selvfølgelig også kaffe, både lys og mørk.</p>
+        <br>
+    </div>
+
+</section>
+
+<script src="js/app.js"></script>
+</body>
+</html>
