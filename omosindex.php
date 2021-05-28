b4b64b7911d9d1a504e67c19b11f38186ecd523f
Natasja <nata3522@edu.easj.com>
2021-05-28 10:55:34 +0200

    Ændring af styles + skalering v. 1.1

--- omosindex.php
+++ omosindex.php
@@ -57,14 +57,19 @@
     <br>
 
     <div class="container">
-        <img src="images/Dea.jpg" height="262" width=100%/>
-        <div class="text__element--centered">
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
 
-                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-                consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                sed diam voluptua.</p><br>
+        <div class="items">
+            <img src="images/Dea.jpg" height="262" width=100%/>
+        </div>
+        <div class="items">
+            <div class="text__element--centered">
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+
+                    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                    consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                    sed diam voluptua.</p><br>
+            </div>
         </div>
 
         <div class="button">
