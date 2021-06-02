7d8924cf1f07af6547dca7f0a85d0a0775ed72fb
TandrupT <tandrupt@gmail.com>
2021-06-02 10:49:39 +0200

    Rulle-links er lavet

--- menu__kort.php
+++ menu__kort.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1
+    ,600;1,700;1,800&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,500;0,600;0,700;0,800;0,900;1,400;
+    1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,15 +21,15 @@
 <?php include 'include/menu.php'?>
 
 <div class="menu-kort">
-
-    <div class="menu__links">
-        <a href="#specials">Specials</a>
-        <a href="#hjemmelavede__sandwiches">Hjemmelavede sandwiches</a>
-        <a href="#kaffe__varme-drikke">Kaffe/varme drikke</a>
-        <a href="#kolde-drikkevare">Kolde drikke</a>
-        <a href="#kager">Kager</a>
-        <a href="#diverse">Diverse</a>
-
+    <div class="menu__links__boks">
+        <div class="menu__links">
+            <a href="#specials">Specials</a>
+            <a href="#hjemmelavede__sandwiches">Hjemmelavede sandwiches</a>
+            <a href="#kaffe__varme-drikke">Kaffe/varme drikke</a>
+            <a href="#kolde-drikkevare">Kolde drikke</a>
+            <a href="#kager">Kager</a>
+            <a href="#diverse">Diverse</a>
+        </div>
     </div>
 
     <!-- Specials -->
